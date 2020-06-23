1e35fd2c1fbe0c4c04da893ee7b4529bc6845faa
michivo <michivo@gmail.com>
2020-06-23 20:09:02 +0000

    Deploying to gh-pages from  @ 57f93236b81f500c6606d94a1e5901a78db63ec4 🚀

---
+++ edfec533.1f24a164.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{162:function(a){a.exports=JSON.parse('{"allTagsPath":"/webbasics/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/webbasics/blog/tags/facebook"}')}}]);
