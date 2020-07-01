9ab30b85a664a4f343070d741b229c093bb2b7bc
Michael Faschinger <michivo@gmail.com>
2020-07-01 17:46:13 +0200

    add about page

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    Einführung: ['intro-onesecond', 'intro-web_vs_internet', 'intro-brief_history', 'intro-protocols', 'intro-dns'],
+    Einführung: ['intro-about', 'intro-onesecond', 'intro-web_vs_internet', 'intro-brief_history', 'intro-protocols', 'intro-dns'],
     HTTP: ['http-intro', 'http-whatfor', 'http-methods', 'http-responses']
   },
 };
