48b252c3848c1cabeabee984364f9b03fa61fd74
michivo <michivo@gmail.com>
2020-10-19 08:50:23 +0000

    Deploying to gh-pages from  @ 8f9debce8f078d322e0bf1757a0e277672073019 🚀

---
+++ e3d32578.296dcf95.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{165:function(s){s.exports=JSON.parse('{"allTagsPath":"/webbasics/blog/tags","slug":"webbasics","name":"webbasics","count":1,"permalink":"/webbasics/blog/tags/webbasics"}')}}]);
