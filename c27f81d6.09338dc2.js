3430b730ad6cdd4b3e8d3ea5936c0afb0455ba8f
michivo <michivo@gmail.com>
2021-01-10 21:44:27 +0000

    Deploying to gh-pages from  @ 7f0237ee858a066a3c8d4b878fd3d53caa11cb78 🚀

--- c27f81d6.09338dc2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[37],{170:function(s){s.exports=JSON.parse('{"hello":{"allTagsPath":"/webbasics/blog/tags","slug":"hello","name":"hello","count":1,"permalink":"/webbasics/blog/tags/hello"},"webbasics":{"allTagsPath":"/webbasics/blog/tags","slug":"webbasics","name":"webbasics","count":1,"permalink":"/webbasics/blog/tags/webbasics"}}')}}]);
